0162eac18abc8e9cccd0206c45420ac729f30fb6
Cipher Lunis <cipheryxinquiries@gmail.com>
2024-01-13 21:05:57 -0600

    Initial Commit

---
+++ CourtJumpSlingshot/CourtJumpSlingshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourtJumpSlingshotApp.swift
+//  CourtJumpSlingshot
+//
+//  Created by Cipher Lunis on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CourtJumpSlingshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
